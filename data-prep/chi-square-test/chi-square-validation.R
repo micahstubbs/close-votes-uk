721ae1a721eb552aabf02c3f7a7f2f1e92946ef1
micahstubbs <micah.stubbs@gmail.com>
2015-06-12 11:16:55 -0500

    further project organizing

--- data-prep/chi-square-test/chi-square-validation.R
+++
@@ -1,5 +0,0 @@
-votes <- matrix(c(28.9, 37.1, 6, 8, 5.2, 7.2, 1.4, 2.1, 0.3, 1.8, 1.2, 23.4, 8.7, 11, 5.4, 11.2, 2.4, 11.2, 0.6, 23.1, 1.3, 1.4),ncol=11,byrow=TRUE)
-colnames(o) <- c("VVD", "PvdA", "PVV", "SP", "CDA", "D66", "CU", "GrLinks", "SGP", "PvdD", "50+")
-rownames(o) <- c("Aa en Hunze"","Aalburg")
-votes <- as.table(votes)
-votes
--- data-prep/map.R
+++
@@ -1,4 +0,0 @@
-library(leaflet)
-m = leaflet(data) %>% addTiles() 
-m = m %>% setView(-2.547855,54.00366, zoom = 5) %>% addCircles(lat = ~ lat, lng = ~ long )
-m
